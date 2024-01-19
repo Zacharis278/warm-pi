012e020a53f0f522b83657446ee38ab10196da1b
Zacharis278 <zhancock@edx.org>
2024-01-19 10:15:55 -0500

    don't use port 80

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def fetch_data():
 app.layout = serve_layout
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=80, debug=False)
+    app.run(host= '0.0.0.0', debug=False)
     # app.run(debug=True)
