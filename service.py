b5ced992fa674ee3e58c480572dd1668762f5dda
Zacharis278 <zhancock@edx.org>
2024-01-30 14:14:34 -0500

    adjust sticky values

--- service.py
+++ service.py
@@ -26,7 +26,7 @@ def continuous_read():
             print(f'actual {value} -- previous {previous_value}')
             # stay sticky to previous reading if within 1 degree (c)
             # this prevents noisy data based on rounding the float
-            if abs(previous_value - value) < 1:
+            if abs(previous_value - value) < 0.5:
                 adjusted_value = previous_value
             previous_value = adjusted_value
             reading = Reading(1, int(time.time()), adjusted_value)
